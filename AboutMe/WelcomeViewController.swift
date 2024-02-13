83c4f800403f74720b393bd0960f5a78dd29c3ac
Piohi <box1234567b@gmail.com>
2024-02-13 13:32:15 +0300

    fix + tabBar

--- AboutMe/WelcomeViewController.swift
+++ AboutMe/WelcomeViewController.swift
@@ -14,10 +14,36 @@ final class WelcomeViewController: UIViewController {
     
     var userName: String!
     
+    private let primaryColor = UIColor (
+        red: 240/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    
+    private let secondaryColor = UIColor (
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 0.9
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameLabel.text = "Welcome, \(userName!)!"
-        greeatingEmoji.text = "👋"
+        userNameLabel.text = "Welcome, \(userName ?? "")!"
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+    }
+}
+
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
 
